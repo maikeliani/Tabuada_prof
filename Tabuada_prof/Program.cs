c48867db01dcc8d68927b549eb95dc4a0e13f268
maikeliani <maikel.iani31@gmail.com>
2023-03-05 22:25:59 -0300

    Add project files.

---
+++ Tabuada_prof/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numero, escolha, resultado;
+        escolha = 0;
+
+        Console.WriteLine(" Informe o numero para a tabuada: ");
+        escolha = int.Parse(Console.ReadLine());
+        Console.Clear();
+        
+            if( ( escolha >= 1) && ( escolha <= 9 ) )
+            {
+                for (numero = 0; numero < 11; numero++)
+                {
+                    resultado = numero * escolha;
+                    Console.WriteLine(escolha + " X " + numero + "= " +(escolha * numero));
+                }
+            }
+            else
+            {
+            Console.WriteLine("Erro! Informe um numero entre 1 e 9!");
+            }
+       
+    }
+}
